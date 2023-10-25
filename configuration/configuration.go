ac1842d7190595b5262837cc25d0c9d5c726a841
Catalin Patulea <catalinp@google.com>
2023-10-24 21:16:17 -0400

    Update default channel to Libera #foulab.

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -1,9 +1,9 @@
 package configuration
 
-const BotChannel = "##foulab"
+const BotChannel = "#foulab"
 const BotNick = "foubot"
 const BotPswd = "HAHAHAHAHA, nope."
-const ServerTLS = "irc.freenode.net:7000"
+const ServerTLS = "irc.libera.chat:6697"
 const StatusEndPoint = "https://foulab.org/YTDMOWI3N2MXNMEZYWE4MGRHYTRLMZC4NJU5MJI2ZJYYODMYNME5NSAGLQO/"
 const Blinker = "http://blinker.lab/"
 
